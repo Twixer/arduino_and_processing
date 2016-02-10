faf0dddb5bded79c93acf2864a4c69b9851e59fe
twixer <twixer@gmail.com>
2016-02-10 23:40:42 +0100

    doc(cnc-plotter) add comments

--- cnc/cnc-plotter/cnc-plotter.ino
+++ cnc/cnc-plotter/cnc-plotter.ino
@@ -1,19 +1,42 @@
 #include <Stepper.h>
- 
+
 const int stepsPerRevolution = 20; 
- 
-//Connection pins:
+const int baudRate = 115200;
+
+//Connection pins
 Stepper myStepperX(stepsPerRevolution, 8,9,10,11); 
- 
+
+
+
+int speed = 100;
+int step = 160; 			//max 250 steps for dvd/cd stepper motors 
+int timeInterval = 100;
+
 void setup() {
- //Set speed:
- myStepperX.setSpeed(100);
- //max 250 steps for dvd/cd stepper motors 
- myStepperX.step(160);
- delay(100);
- 
+
+	// initialize the serial port:
+	Serial.begin(baudRate);
+
+	Serial.println("Starting setup()");
+	
+	// ...
+
+	Serial.println("Ending setup()");
 }
- 
+
 void loop() {
 
+	Serial.println("Starting loop()");
+	
+	Serial.println("Speed 100");
+	Serial.println("Step 160");
+
+	myStepperX.setSpeed(speed);
+	myStepperX.step(step);
+	delay(timeInterval);
+
+	myStepperX.step(-step);
+	delay(timeInterval);
+
+	Serial.println("Ending loop()");
 }
